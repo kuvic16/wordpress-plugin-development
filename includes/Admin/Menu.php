c403f0cc51874a791352839c44208e0d95faddf2
Shaiful Islam <kuvic16@gmail.com>
2020-04-13 13:18:11 +0600

    added submenu

--- includes/Admin/Menu.php
+++ includes/Admin/Menu.php
@@ -6,18 +6,57 @@ namespace Plugin\Dev\Admin;
  * The menu handler class
  */
 class Menu{
+
+    /**
+     * Initialize the menu class
+     */
     function __construct(){
         add_action('admin_menu', [$this, 'admin_menu']);
     }
 
+    /**
+     * Register admin menu
+     * 
+     * @return void
+     */
     public function admin_menu(){
-        add_menu_page(__('Plugin Dev', 'plugin-dev'), 
-        __('Dev', 'plugin-dev'), 'manage_options', 
-        'plugin-dev', [$this, 'plugin_page'], 
-        'dashicons-welcome-learn-more');
+        $parent_slug = 'plugin-dev';
+        $capability = 'manage_options';
+
+        add_menu_page(
+            __('Plugin Dev', 'plugin-dev'), 
+            __('Plugin Dev', 'plugin-dev'), 
+            $capability, 
+            $parent_slug, 
+            [$this, 'plugin_page'], 
+            'dashicons-welcome-learn-more'
+        );
+
+        add_submenu_page(
+            $parent_slug, 
+            __('Address Book', 'plugin_dev'), 
+            __('Address Book', 'plugin_dev'), 
+            $capability, 
+            'plugin-dev-addressbook', 
+            [$this, 'addressbook_page']
+        );
     }
 
+    /**
+     * Render the plugin page
+     * 
+     * @return void
+     */
     public function plugin_page(){
         echo 'Hello World';
+    }
+
+    /**
+     * Render the address book page
+     * 
+     * @return void
+     */
+    public function addressbook_page(){
+        echo 'Addressbook page';
     }
 }
