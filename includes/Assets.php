acf3c65d16380009064e340515c3a0d9c3a6fd10
Shaiful Islam <kuvic16@gmail.com>
2020-04-26 16:12:42 +0600

    minor changes made: used visual code alignment

--- includes/Assets.php
+++ includes/Assets.php
@@ -24,19 +24,19 @@ class Assets{
     public function get_scripts(){
         return [
             'plugin-dev-script' => [
-                'src' => P_DEV_ASSETS . '/js/frontend.js',
+                'src'     => P_DEV_ASSETS . '/js/frontend.js',
                 'version' => filemtime(P_DEV_PATH . '/assets/js/frontend.js'),
-                'deps' => ['jquery']
+                'deps'    => ['jquery']
             ],
             'plugin-dev-enquiry-script' => [
-                'src' => P_DEV_ASSETS . '/js/enquiry.js',
+                'src'     => P_DEV_ASSETS . '/js/enquiry.js',
                 'version' => filemtime(P_DEV_PATH . '/assets/js/enquiry.js'),
-                'deps' => ['jquery']
+                'deps'    => ['jquery']
             ],
             'plugin-dev-admin-script' => [
-                'src' => P_DEV_ASSETS . '/js/admin.js',
+                'src'     => P_DEV_ASSETS . '/js/admin.js',
                 'version' => filemtime(P_DEV_PATH . '/assets/js/admin.js'),
-                'deps' => ['jquery', 'wp-util']
+                'deps'    => ['jquery', 'wp-util']
             ]
         ];
     }
@@ -49,15 +49,15 @@ class Assets{
     public function get_styles(){
         return [
             'plugin-dev-style' => [
-                'src' => P_DEV_ASSETS . '/css/frontend.css',
+                'src'     => P_DEV_ASSETS . '/css/frontend.css',
                 'version' => filemtime(P_DEV_PATH . '/assets/css/frontend.css')
             ],
             'plugin-dev-admin-style' => [
-                'src' => P_DEV_ASSETS . '/css/admin.css',
+                'src'     => P_DEV_ASSETS . '/css/admin.css',
                 'version' => filemtime(P_DEV_PATH . '/assets/css/admin.css')
             ],
             'plugin-dev-enquiry-style' => [
-                'src' => P_DEV_ASSETS . '/css/enquiry.css',
+                'src'     => P_DEV_ASSETS . '/css/enquiry.css',
                 'version' => filemtime(P_DEV_PATH . '/assets/css/enquiry.css')
             ]
         ];
@@ -83,7 +83,7 @@ class Assets{
 
         $styles = $this->get_styles();
         foreach($styles as $handle => $style){
-            $deps = isset($style['deps']) ? $style['deps'] : false;
+            $deps = isset($style['deps']) ? $style['deps']: false;
             wp_register_style(
                 $handle, 
                 $style['src'], 
@@ -94,13 +94,13 @@ class Assets{
 
         wp_localize_script('plugin-dev-enquiry-script', 'pluginDev',[
             'ajaxurl' => admin_url('admin-ajax.php'),
-            'error' => __('Something went wrong', 'plugin-dev')
+            'error'   => __('Something went wrong', 'plugin-dev')
         ]);
 
         wp_localize_script('plugin-dev-admin-script', 'pluginDev',[
-            'nonce' => wp_create_nonce('wd-pd-admin-nonce'),
+            'nonce'   => wp_create_nonce('wd-pd-admin-nonce'),
             'confirm' => __('Are you sure?', 'plugin-dev'),
-            'error' => __('Something went wrong', 'plugin-dev')
+            'error'   => __('Something went wrong', 'plugin-dev')
         ]);
 
     }
